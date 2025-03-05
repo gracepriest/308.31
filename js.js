5b6d151ba298360acd1b1355d8dcb248a022bfed
gracepriest <melvinkempjr3@outlook.com>
2025-03-05 16:16:39 -0500

    update

--- js.js
+++ js.js
@@ -24,3 +24,42 @@ document.write("<br>")
 }
 document.write("</div>") 
  document.write("</div>")       
+
+
+ document.write('<div class="container"><div class="card float-start"><div class="card-header"><h2>Part 1</h2></div>')
+document.write('<div class="card-body">')
+//using while loop instead
+// let n = 1
+// while(n < 101 )
+// {
+
+// if(n % 5 == 0 && n % 3 == 0)
+//     {
+//         document.write("Number " + n + " is: ")
+//         document.write("Fizz Buzz")
+//         document.write("<br>")
+//     }
+// else if(n % 3 == 0)
+// {
+//     document.write("Number " + n + " is: ")
+//     document.write("Fizz")
+//     document.write("<br>")
+// }
+// else if(n % 5 == 0)
+//     {
+//         document.write("Number " + n + " is: ")
+//         document.write("Buzz")
+//         document.write("<br>")
+//     }
+//     else{
+//         //IF YOU WANT TO DISPLAY ALL NUMBERS
+//         //uncomment
+//         //document.write("Number " + n + " is: ")
+//         //document.write("<br>")
+//     }
+     
+
+// n++
+// }
+// document.write("</div>") 
+// document.write("</div>")       
